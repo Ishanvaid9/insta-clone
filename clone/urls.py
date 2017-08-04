c173d8746bc356eb4333887d24d6fdea9eee09c4
vaidishan9@gmail.com <vaidishan9@gmail.com>
2017-08-04 12:44:26 +0530

    ading of feed page

--- clone/urls.py
+++ clone/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from views import singnup_view,login_user
+from views import singnup_view,login_user,post_view,feed_view,like_view,comment_view
 
 urlpatterns = [
     url(r'^$', singnup_view),
-    url(r'^login/',login_user)
+    url(r'^login/',login_user),
+    url(r'^post/', post_view),
+    url(r'^feed/', feed_view),
+    url(r'^like/', like_view),
+    url(r'^comment/', comment_view),
 ]
--- clone/views.py
+++ clone/views.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from datetime import datetime
-from forms import SignUpForm,LoginForm
-from models import UserModel,SessionToken
+from forms import SignUpForm,LoginForm,PostForm,LikeForm,CommentForm
+from models import UserModel,SessionToken,PostModel,LikeModel,CommentModel
 from django.contrib.auth.hashers import make_password,check_password
 from django.shortcuts import render,redirect
+from imgurpython import ImgurClient
+import os
+from datetime import timedelta
+from django.utils import timezone
 # Create your views here.
+
+
+CLIENT_ID='e8b96d3df82469'
+CLIENT_SECRET= 'f6292d93b81e0f055521eb71084b63b9ccc5329d'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 def singnup_view(request):
         print ' view called'
         if request.method == "POST":
@@ -59,8 +70,32 @@ def login_user(request):
     return render(request, 'login.html', response_data)
 
 
-def feed_view(request):
-    return render(request, 'feed.html')
+
+
+def post_view(request):
+        user = check_validation(request)
+        print "post view called"
+        if user:
+            print 'Authentic user'
+            if request.METHOD == 'GET':
+                form = PostForm()
+            elif request.method == 'POST':
+                form = PostForm(request.POST, request.FILES)
+                if form.is_valid():
+                    image = form.cleaned_data.get('image')
+                    caption = form.cleaned_data.get('caption')
+
+                    post = PostModel(user=user, image=image, caption=caption)
+                    post.save()
+                    path =   BASE_DIR+'/'+ post.image.url
+                    print path
+                    client = ImgurClient(CLIENT_ID,CLIENT_SECRET)
+                    post.image_url = client.upload_from_path(path, anon=True)['link']
+                    post.save()
+            return render(request, 'feed.html', {'form': form})
+
+        else:
+            return redirect('/login/')
 
 
 # For validating the session
@@ -72,3 +107,58 @@ def check_validation(request):
     else:
         return None
 
+
+def feed_view(request):
+    user = check_validation(request)
+    if user:
+        posts = PostModel.objects.all().order_by('created_on')
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+        return redirect('/login/')
+
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+
+            existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
+
+            if not existing_like:
+                LikeModel.objects.create(post_id=post_id, user=user)
+            else:
+                existing_like.delete()
+
+            return redirect('/feed/')
+
+    else:
+        return redirect('/login/')
+
+
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
+            comment.save()
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
+        return redirect('/login')
+
+
+# For validating the session
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
+                return session.user
+    else:
+        return None
